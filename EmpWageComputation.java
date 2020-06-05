2505a606d0c91cda5b78904e8b0d0dc11d272718
Mikhil Desai <mikhildesai@gmail.com>
2020-06-05 20:47:47 +0530

    Refactored the code and used array to store different companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,25 +1,71 @@
-class EmpWageBuilder {
+
+class CompanyEmpWage{
 
 	public final String company;
 	public final int wage_per_hour,
 			max_hours,
 			maxhrs_month;
+	public int total_emp_wage;
 
-	EmpWageBuilder(String company,int wage_per_hour,int max_hours,int maxhrs_month)
+	CompanyEmpWage(String company,int wage_per_hour,int max_hours,int maxhrs_month)
 	{
 		this.company=company;
 		this.wage_per_hour=wage_per_hour;
 		this.max_hours=max_hours;
 		this.maxhrs_month=maxhrs_month;
-	} 
+		total_emp_wage=0;
+	}
 
-	public int wagecalculation(){
+	public String toString(){
+
+		return "Total Empolyee wage for company"+company+"is"+total_emp_wage;
+	}
+
+	public void  setTotalWage(int total_emp_wage){
+
+			this.total_emp_wage=total_emp_wage;
+	}
+
+}
+
+
+
+class EmpWageBuilder {
+
+
+	public CompanyEmpWage[] companyEmpWageArray;
+	int count=0;
+
+	EmpWageBuilder()
+	{
+		companyEmpWageArray = new CompanyEmpWage[5];
+	}
+
+
+
+	public void addCompany(String company,int wage_per_hour,int max_hours, int maxhrs_month)
+	{
+		companyEmpWageArray[count]=new CompanyEmpWage(company,wage_per_hour,max_hours,
+								maxhrs_month);
+		count++;
+	}
+
+	public void wagecalculation()
+	{
+			for(int i=0;i<count;i++)
+			{
+			companyEmpWageArray[i].setTotalWage(this.wagecalculation( companyEmpWageArray[i]) );
+			System.out.println(companyEmpWageArray[i]);
+			}
+	}
+
+	public int wagecalculation(CompanyEmpWage Cobj){
 
 		int totalwage=0,total_emphrs=0,total_working_days=0, 
 			wage=0 , emphrs=0;
 
-		while (total_emphrs<max_hours && total_working_days < maxhrs_month)
-	 {
+		while (total_emphrs < Cobj.max_hours && total_working_days < Cobj.maxhrs_month)
+	 	{
 			total_working_days++;
 			int empcheck = (int)(Math.random()*10)%3;
 
@@ -36,13 +82,15 @@ class EmpWageBuilder {
 
 			}
 				total_emphrs +=emphrs;
-				wage = wage_per_hour*emphrs;
-				System.out.println("Employee wage="+wage);
+				wage = Cobj.wage_per_hour*emphrs;
 				totalwage += wage;
+				System.out.println("Day " +total_working_days+ " Employee hrs "+emphrs+
+							" TotalWage=  "+totalwage);
+
 		}
-			System.out.println("Company "+company+"Monthly wage is="+totalwage);
-			return totalwage;
-		}
+
+			return  total_emphrs * Cobj.wage_per_hour;
+	}
 
 }
 
@@ -54,11 +102,11 @@ class EmpWageComputation{
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		EmpWageBuilder obj1 = new EmpWageBuilder("Flipkart",40,100,33);
+		EmpWageBuilder obj1 = new EmpWageBuilder();
+		obj1.addCompany("Flipkart",44,111,3);
+		obj1.addCompany("Amazon",34,121,4);
+		//obj1.toString();
 		obj1.wagecalculation();
-		
-		EmpWageBuilder obj2 = new EmpWageBuilder("Amazon",34,121,44);
-		obj2.wagecalculation();
 
 	}
 
